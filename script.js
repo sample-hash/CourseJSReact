1f98a3f2c75e2ab4d9f19840efd788869f48f189
Andrii Kovalenkov <andrii.kovalenkov@gmail.com>
2023-01-09 15:24:10 -0800

    Added new task

--- script.js
+++ script.js
@@ -43,7 +43,39 @@ function func2(){
 }
 
 function func3() {
-    
+    const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+
+    if(personalMovieDB.count < 10){
+        alert('Просмотрено мало фильмов');
+    }else if(personalMovieDB.count < 30){
+        alert('Вы классический зритель');
+    } else if(personalMovieDB.count > 30){
+        alert('Вы киноман');
+    } else {
+        alert('Произошла ошибка');
+    }
+
+    for(let i = 0; i < 2; i++){
+        const movie = prompt('Один из последних просмотренных фильмов?', ''),
+        rate = prompt('На сколько его оцените?', '');
+
+        if(movie != '' && rate != '' && movie != null && rate != null && movie.length < 50){
+            personalMovieDB.movies[movie] = rate;
+        } else {
+            console.log('Error');
+            i--;
+        }
+    }
+
+    console.log(personalMovieDB);
 }
 
-console.log(func2());
+console.log(func3());
