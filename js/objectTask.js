ab2039006b8089a652dbed36bb0a9d0d1d6d345e
Andrii Kovalenkov <andrii.kovalenkov@gmail.com>
2023-01-14 00:13:57 -0800

    Object tasks

--- js/objectTask.js
+++ js/objectTask.js
@@ -1,2 +1,37 @@
 "use strict";
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function(data){
+        const {age} = data;
+        const {languages} = data.skills;
+        let result = `Мне ${age} и я владею языками: `;
+        languages.forEach(function(lang){
+            result += `${lang.toUpperCase()} `;
+        });
+        return result;
+    }
+};
+console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
 
+function showProgrammingLangs(data){
+    let result = '';
+    const {programmingLangs} = data.skills;
+    for(let key in programmingLangs){
+        result += `Язык ${key} изучен на ${programmingLangs[key]} `;
+    }
+    return result;
+}
+console.log(showProgrammingLangs(personalPlanPeter));
+
+function showExperience(data){
+    return data.skills.exp; 
+}
