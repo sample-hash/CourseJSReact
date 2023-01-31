088dc47122f3b1799fef2c16250ca491a24e1949
Andrii Kovalenkov <andrii.kovalenkov@gmail.com>
2023-01-30 21:44:21 -0800

    Task 47. Events practice.

--- part2/project/js/script.js
+++ part2/project/js/script.js
@@ -16,11 +16,11 @@
 
 const movieDB = {
     movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
+        "Ааа",
+        "Яяя",
+        "Ггг",
+        "Ввв",
+        "Ллл"
     ]
 };
 
@@ -28,7 +28,8 @@ const adv = document.querySelectorAll('.promo__adv img'),
       genre = document.querySelector('.promo__genre'),
       bg = document.getElementsByClassName('promo__bg'),
       lista = document.querySelector('.promo__interactive-list'),
-      li = document.querySelectorAll('.promo__interactive-item');
+      li = document.querySelectorAll('.promo__interactive-item'),
+      confirmBtn = document.querySelector('button');
 
 adv.forEach(item => {
     item.remove();
@@ -39,15 +40,47 @@ bg[0].style.backgroundImage = 'url("img/bg.jpg")';
 
 lista.innerHTML = '';
 
-movieDB.movies.sort().forEach((item, index) => {
-    const div = document.createElement('div');
-    div.classList.add('delete');
-
-    const li = document.createElement('li');
-    li.classList.add('promo__interactive-item');
-    li.textContent = `${index+1}. ${item}`;
-    li.append(div);
-    lista.append(li);
+confirmBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const inputValue = document.querySelector('.adding__input').value;
+    const checkboxStatus = document.querySelector('input[type="checkbox"]').checked;
+    if(checkboxStatus){
+        console.log('Добавляем любимый фильм');
+    }
+    if(inputValue.length > 21){
+        let newName = inputValue.slice(0, 21);
+        newName += '...';
+        movieDB.movies.push(newName);
+    } else {
+        movieDB.movies.push(inputValue);
+    }
+    lista.innerHTML = '';
+    updateArrList();
 });
+
+function updateArrList(){
+    movieDB.movies.sort().forEach((item, index) => {
+        const div = document.createElement('div');
+        div.classList.add('delete');
+    
+        const li = document.createElement('li');
+        li.classList.add('promo__interactive-item');
+        li.textContent = `${index+1}. ${item}`;
+        li.append(div);
+        lista.append(li);
+    });
+
+    document.querySelectorAll('.delete').forEach((btn, index) => {
+        btn.addEventListener('click', () => {
+            btn.parentElement.remove();
+            movieDB.movies.splice(index, 1);
+        });
+    });
+}
+
+updateArrList();
+
+
+
 
 
